8725582ad8f0eb19affca6d38dac2c8efc9b2470
tatiana110504 <60788560+tatiana110504@users.noreply.github.com>
2020-03-10 11:12:19 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.listen()
+async def on_ready():
+    print('Bot loaded and ready!')
+
+@bot.command(name="hi", help="Says hello")
+async def say_hello(ctx):
+    await ctx.send(f"Hi {ctx.author.display_name}")
+
+bot.run(TOKEN)
