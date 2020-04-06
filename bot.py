065c9698a2e3f699ed8dcbd9f681559d70180a0e
tatiana110504 <60788560+tatiana110504@users.noreply.github.com>
2020-04-06 19:41:32 -0400

    commit

--- bot.py
+++ bot.py
@@ -16,5 +16,42 @@ async def on_ready():
 @bot.command(name="hi", help="Says hello")
 async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
+@bot.command()
+async def reservations(ctx):
+    await ctx.send(while restart != ('N','NO','n','no'):
+print("RESREVE")
+restart = ("Y")
 
+while restart != ('N','NO','n','no'):
+    print("1.Check pnr status")
+    print("2.Table and Game Reservation")
+    option = int(input("\nEnter your option : "))
+    
+    if option == 1:
+        print("Your pnr status is t2")
+        exit(0)
+  
+    elif option == 2:
+        people = int(input("\nEnter no. of people at table: "))
+        name_l = []
+        game_l = []
+        table_l = []
+        for p in range(people):2
+        name = str(input("\nName : "))
+        name_l.append(name)
+        game = str(input("\nGame  : "))
+        game_l.append(game)
+        table  = str(input("\nTable number : "))
+        table_l.append(table)
+
+        restart = str(input("\nOops! Forget someone : "))
+        if restart in ('y','YES','yes','Yes'):
+                restart = ('Y')
+        else:
+                    x=0
+        print("People : ",people)
+        print("Name : ", name_l[x])
+        print("Game  : ", game_l[x])
+        print("Table : ", table_l[x])
+        x += 1
 bot.run(TOKEN)
